e1a73b0e3e5fd64e92d85b62b4528192bb8f9cce
Sosko Aleksey <sosko.aleksey@gmail.com>
2022-06-17 15:01:11 +0700

    Diplom0.1

---
+++ src/main/java/ru/netology/javacore/Request.java
@@ -0,0 +1,19 @@
+package ru.netology.javacore;
+
+public class Request {
+    private String type;
+    private String task;
+
+    public Request(String type, String task) {
+        this.type = type;
+        this.task = task;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getTask() {
+        return task;
+    }
+}
